b7d96d6349d86ffe03046a7ab2df88cbaa12229d
Brian LeRoux <b@brian.io>
2017-05-14 20:37:19 -0700

    fixes json response

--- src/http/json/_response.js
+++ src/http/json/_response.js
@@ -3,22 +3,22 @@ var session = require('../session').client(process.env.SESSION_TABLE_NAME || 'ar
 module.exports = function response(request, callback, cmds) {
 
   // ensure only valid command keys
-  var allowed = ['location', 'session', 'html']
+  var allowed = ['location', 'session', 'json']
   Object.keys(cmds).forEach(k=> {
     if (!allowed.includes(k)) {
-      throw Error(k + ' unknown key. Only location, session and html allowed')
+      throw Error(k + ' unknown key. Only location, session and json allowed')
     }
   })
 
   // ensure not both location and html
-  var hasLocationAndHtml = cmds.hasOwnProperty('location') && cmds.hasOwnProperty('html')
-  if (hasLocationAndHtml) {
-    throw Error('Found location and html keys; only one is allowed')
+  var hasLocationAndJson = cmds.hasOwnProperty('location') && cmds.hasOwnProperty('json')
+  if (hasLocationAndJson) {
+    throw Error('Found location and json keys; only one is allowed')
   }
-  // ensure one of location or html
-  var hasOneOfLocationOrHtml = cmds.hasOwnProperty('location') || cmds.hasOwnProperty('html')
-  if (!hasOneOfLocationOrHtml) {
-    throw Error('response must have location or html')
+  // ensure one of location or json
+  var hasOneOfLocationOrJson = cmds.hasOwnProperty('location') || cmds.hasOwnProperty('json')
+  if (!hasOneOfLocationOrJson) {
+    throw Error('response must have location or json')
   }
 
   var sesh = Object.assign(cmds.session || request.session, {_idx:request._idx})
