cf597de0cf381adb98f6e6ae5406e2354967bfe9
Brian LeRoux <b@brian.io>
2017-06-16 15:57:47 -0700

    fix for mock

--- src/http/session/_get-dynamo-doc-instance.js
+++ src/http/session/_get-dynamo-doc-instance.js
@@ -4,7 +4,7 @@ var passthru = !process.env.hasOwnProperty('SESSION_TABLE_NAME')
 var endpoint = new aws.Endpoint('http://localhost:5000')
 
 // if SESSION_TABLE_NAME isn't defined we mock the client and just pass session thru
-var passthru = {
+var mock = {
   get(params, callback) {
     callback()
   },
