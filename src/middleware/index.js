52c5ada9a5bcc619e4ccb6b7612c2a11d19f77d5
Mike MacCana <mike@certsimple.com>
2018-12-11 16:37:52 +0000

    Add new await-based middleware as arc.middleware

---
+++ src/middleware/index.js
@@ -0,0 +1,36 @@
+// Create a combined function from the middleware steps provided
+// Each step is a regular Arc 4 async/await route function, that may return
+//  - A falsy value - keep processing, ie move onto the next step
+//  - A modified request - keep processing, passing the modified request onto subsequent steps
+//  - A response - end processing and respond to the client
+function addMiddleware(...steps) {
+  let combined = async function(request) {
+    // Running combined function!
+    var response
+    for (let step of steps) {
+      // Running step ${step.name}
+      var middleWareResult = await step(request)
+      var isRequest = middleWareResult && middleWareResult.hasOwnProperty('method')
+      if (isRequest) {
+        // Middleware ${step.name} has returned a modified request, continuing...
+        request = middleWareResult
+      } else {
+        if (middleWareResult) {
+          response = middleWareResult
+          // Got a response from ${step.name}, finishing...
+          break
+        }
+        // Did not get a result from ${step.name}, continuing...
+      }
+    }
+    // Finished combined function!
+    return response
+  }
+  var stepNames = steps.map(function(step) {
+    return step.name
+  })
+  // Combined functions ${stepNames}
+  return combined
+}
+
+module.exports = addMiddleware
