e8d1ea3e06445c27c89e70aa2390ee6b93644510
Brian LeRoux <b@brian.io>
2019-02-01 01:47:33 -0800

    adds arc.proxy.public({spa}) option

--- src/proxy/public.js
+++ src/proxy/public.js
@@ -7,20 +7,28 @@ let headers = {
   'Content-Type': 'text/html; charset=utf8'
 }
 
-module.exports = function proxyPublic() {
+module.exports = function proxyPublic({spa}) {
   return async function proxy(req) {
     try {
-      // Lookup the Key w smarts for index.html (this needs improvement)
-      let Key = req.path === '/'? 'index.html' : req.path.substring(1)
-      if (Key != 'index.html' && req.path.lastIndexOf('/') === req.path.length - 1) {
-        Key = Key + 'index.html'
+      let Key
+      if (spa) {
+        let isFolder = req.path.indexOf('.') === -1
+        Key = isFolder? 'index.html' : req.path.substring(1)
+      }
+      else {
+        // Lookup the Key w smarts for index.html (this needs improvement)
+        Key = req.path === '/'? 'index.html' : req.path.substring(1)
+        // add index.html to any empty folder path
+        if (Key != 'index.html' && req.path.lastIndexOf('/') === req.path.length - 1) {
+          Key = Key + 'index.html'
+        }
       }
 
       // overide Content-Type by Key mime-type
       headers['Content-Type'] = mime.contentType(path.extname(Key))
 
       // read the blob
-      let body = await read({Key})
+      let body = await read(Key)
 
       // return the blob
       return {headers, body}
