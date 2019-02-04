a123fd1180eb5ec8c8e5f2115d0a4f47141a3653
Brian LeRoux <b@brian.io>
2019-02-04 13:02:29 -0800

    adds missing callback

--- src/http/session/read.js
+++ src/http/session/read.js
@@ -4,7 +4,7 @@ let ddb = require('./providers/ddb')
 module.exports = function read(request, callback) {
 
   if (process.env.SESSION_TABLE_NAME === 'jwe')
-    return jwe.read(request)
+    return jwe.read(request, callback)
 
   return ddb.read(request, callback)
 }
--- src/http/session/write.js
+++ src/http/session/write.js
@@ -4,7 +4,7 @@ let ddb = require('./providers/ddb')
 module.exports = function write(params, callback) {
 
   if (process.env.SESSION_TABLE_NAME === 'jwe')
-    return jwe.write(params)
+    return jwe.write(params, callback)
 
   return ddb.write(params, callback)
 }
