d1ab6942c94b1e52873c8b32ce65464406c0ce5f
Brian LeRoux <b@brian.io>
2019-04-19 18:15:40 -0700

    new! arc.events.subscribe now accepts an AsyncFunction; thx @alexbepple closes arc-repos/arc-functions#34

--- src/events/_subscribe.js
+++ src/events/_subscribe.js
@@ -12,20 +12,36 @@ var parallel = require('run-parallel')
  * exports.handler = arc.events.subscribe(signup)
  */
 module.exports = function _subscribe(fn) {
-  return function _lambdaSignature(evt, ctx, callback) {
-    // sns triggers send batches of records
-    // so we're going to create a handler for each one
-    // and execute them in parallel
-    parallel(evt.Records.map(function _iterator(record) {
-      // for each record we construct a handler function
-      return function _actualHandler(callback) {
+  if (fn.constructor.name === 'AsyncFunction') {
+    return async function lambda(event) {
+      return await Promise.all(event.Records.map(async record=> {
         try {
-          fn(JSON.parse(record.Sns.Message), callback)
+          let result = JSON.parse(record.Sns.Message)
+          return await fn(result)
         }
         catch(e) {
-          callback(e)
+          throw e
         }
-      }
-    }), callback)
+      }))
+    }
+  }
+  else {
+    // callback interface
+    return function _lambdaSignature(evt, ctx, callback) {
+      // sns triggers send batches of records
+      // so we're going to create a handler for each one
+      // and execute them in parallel
+      parallel(evt.Records.map(function _iterator(record) {
+        // for each record we construct a handler function
+        return function _actualHandler(callback) {
+          try {
+            fn(JSON.parse(record.Sns.Message), callback)
+          }
+          catch(e) {
+            callback(e)
+          }
+        }
+      }), callback)
+    }
   }
 }
