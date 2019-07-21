3b03d9a4cefad00cca1a1ec7ee0b0ed1538d75c6
filmaj <maj.fil@gmail.com>
2019-07-21 12:23:37 -0500

    ✅🐛 fix for middleware functionality on lambda. added test for unmodified request passing in middleware.

--- test/unit/middleware/index-test.js
+++ test/unit/middleware/index-test.js
@@ -14,25 +14,41 @@ test('arc.middleware should allow for munging and passing a request object betwe
     return { statusCode: 200, body: req.body }
   }
   let handler = middleware(one, two)
-  handler({method: 'GET'})
+  handler({httpMethod: 'GET'})
 })
+
 test('arc.middleware should prevent further middleware processing when a response is returned', t => {
   t.plan(1)
   let one = function () { return { statusCode: 200 } }
   let two = sinon.fake()
   let handler = middleware(one, two)
-  handler({method: 'GET'})
+  handler({httpMethod: 'GET'})
   t.notOk(two.callCount, 'second middleware not called')
 })
+
 test('arc.middleware should throw if no middleware returns a response', async t => {
   t.plan(1)
   let one = function (req) { return req }
   let two = function (req) { return req }
   let handler = middleware(one, two)
   try {
-    await handler({method: 'GET'})
+    await handler({httpMethod: 'GET'})
   } catch (e) {
     t.ok(e, 'exception thrown')
     t.end()
   }
+})
+
+test('arc.middleware should pass original request if layer does not return anything', async t => {
+  t.plan(1)
+  let one = function () { return }
+  let two = sinon.fake.returns({statusCode: 200})
+  let request = {httpMethod: 'GET', querystringParameters: {q: 'poop'}}
+  let handler = middleware(one, two)
+  try {
+    await handler(request)
+  } catch (e) {
+    t.fail(e, 'exception thrown')
+  }
+  t.ok(two.calledWith(request), 'second middleware layer called with origin request object')
 })
