0b6edc53ecd4965ce8102d3fc154556bbc782869
filmaj <maj.fil@gmail.com>
2019-07-22 16:25:44 -0500

    ✅ unskip + fix up proxy.read test

--- test/unit/http/proxy/read-test.js
+++ test/unit/http/proxy/read-test.js
@@ -1,24 +1,22 @@
 let test = require('tape')
 let sinon = require('sinon')
-let fs = require('fs')
+// will need aws-sdk-mock to test out the rest of the read module
+// let AWS = require('aws-sdk-mock')
 // we need to reset the node_env for each test, to test different behaviour, and
 // the node_env gets inspected at the top/global level of the module, therefore
 // we set noPreserveCache and load the read module within each test.
 let proxyquire = require('proxyquire').noPreserveCache();
 const NODE_ENV = process.env.NODE_ENV
-let readFake = sinon.stub(fs, 'readFile').yields(null, 'this is file body')
 
-test.skip('arc.proxy.read should read from local public folder (via the transform module) when in testing mode', async t => {
+test('arc.proxy.read should invoke local sandbox when in testing mode', async t => {
   process.env.NODE_ENV = 'testing'
-  let transformStub = sinon.stub().returns(true)
+  let sandboxStub = sinon.stub().resolves(true)
   let read = proxyquire('../../../../src/http/proxy/read', {
-    './transform': transformStub
+    './sandbox': sandboxStub
   })
   t.plan(1)
-  let existsFake = sinon.stub(fs, 'existsSync').returns(true)
-  await read({ Key: 'index.html' })
-  t.ok(transformStub.calledOnce, 'transform function called')
-  readFake.restore()
-  existsFake.restore()
+  let params = { Key: 'index.html', config: {short:true} }
+  await read(params)
+  t.ok(sandboxStub.calledWith(params), 'sandbox function called with expected parameters')
   process.env.NODE_ENV = NODE_ENV
 })
