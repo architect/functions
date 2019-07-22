13d8e54868a2f63e1d4edf87f0d188bd9844a284
filmaj <maj.fil@gmail.com>
2019-07-22 13:04:01 -0500

    moving tests around to address files moved, skipping one of the tests

---
+++ test/unit/http/middleware/index-test.js
@@ -0,0 +1,54 @@
+let test = require('tape')
+let sinon = require('sinon')
+let middleware = require('../../../../src/http/middleware')
+
+test('arc.middleware should allow for munging and passing a request object between different middleware layers', t => {
+  t.plan(1)
+  let one = function (req) {
+    req.body = req.body || {}
+    req.body.munge = true
+    return req
+  }
+  let two = function (req) {
+    t.ok(req.body.munge, 'request object was munged and passed through middleware')
+    return { statusCode: 200, body: req.body }
+  }
+  let handler = middleware(one, two)
+  handler({httpMethod: 'GET'})
+})
+
+test('arc.middleware should prevent further middleware processing when a response is returned', t => {
+  t.plan(1)
+  let one = function () { return { statusCode: 200 } }
+  let two = sinon.fake()
+  let handler = middleware(one, two)
+  handler({httpMethod: 'GET'})
+  t.notOk(two.callCount, 'second middleware not called')
+})
+
+test('arc.middleware should throw if no middleware returns a response', async t => {
+  t.plan(1)
+  let one = function (req) { return req }
+  let two = function (req) { return req }
+  let handler = middleware(one, two)
+  try {
+    await handler({httpMethod: 'GET'})
+  } catch (e) {
+    t.ok(e, 'exception thrown')
+    t.end()
+  }
+})
+
+test('arc.middleware should pass original request if layer does not return anything', async t => {
+  t.plan(1)
+  let one = function () { return }
+  let two = sinon.fake.returns({statusCode: 200})
+  let request = {httpMethod: 'GET', querystringParameters: {q: 'poop'}}
+  let handler = middleware(one, two)
+  try {
+    await handler(request)
+  } catch (e) {
+    t.fail(e, 'exception thrown')
+  }
+  t.ok(two.calledWith(request), 'second middleware layer called with origin request object')
+})
---
+++ test/unit/queues/subscribe-test.js
@@ -0,0 +1,23 @@
+let test = require('tape')
+let sinon = require('sinon')
+let subscribe = require('../../../src/queues/subscribe')
+let mockSqsEvent = require('./mock-sqs-event.json')
+
+test('queues.subscribe calls handler', t=> {
+  t.plan(1)
+
+  let eventHandler = sinon.fake.yields()
+
+  // get a lambda signature from the handler
+  let handler = subscribe(eventHandler)
+
+  // invoke the lambda handler with mock payloads
+  let mockContext = {}
+  handler(mockSqsEvent, mockContext, function _handler(err) {
+    if (err) {
+      t.fail(err)
+    } else {
+      t.ok(eventHandler.calledOnce, 'event handler called once')
+    }
+  })
+})
