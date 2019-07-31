17a4703a0fc4eed7ca9e4e726f06a81815e632e7
Brian LeRoux <b@brian.io>
2019-07-31 16:02:27 -0700

    updates proxy to use ARC_SANDBOX_PATH_TO_STATIC env

--- src/http/proxy/sandbox.js
+++ src/http/proxy/sandbox.js
@@ -6,16 +6,20 @@ let readFile = util.promisify(fs.readFile)
 let transform = require('./transform')
 
 module.exports = async function sandbox({Key, config}) {
+  // additive change... after 6.x we can rely on this env var in sandbox
+  let basePath = process.env.ARC_SANDBOX_PATH_TO_STATIC || path.join(process.cwd(), '..', '..', '..', 'public')
+
+  // Lookup the blob in ./public
+  // assuming we're running from a lambda in src/**/* OR from vendored node_modules/@architect/sandbox
+  let filePath = path.join(basePath, Key)
+
   try {
-    // Lookup the blob in ./public
-    // assuming we're running from a lambda in src/**/*
-    let filePath = path.join(process.cwd(), '..', '..', '..', 'public', Key)
     if (!fs.existsSync(filePath))
       throw ReferenceError(`${filePath} not found`)
 
-    // read the file
     let body = await readFile(filePath, {encoding: 'utf8'})
     let type = mime.contentType(path.extname(Key))
+
     return transform({
       Key,
       config,
@@ -26,9 +30,9 @@ module.exports = async function sandbox({Key, config}) {
     })
   }
   catch(e) {
-    //look for public/404.html
+    // look for public/404.html
     let headers = {'content-type': 'text/html; charset=utf8;'}
-    let http404 = path.join(process.cwd(), '..', '..', '..', 'public', '404.html')
+    let http404 = path.join(basePath, '404.html')
     let exists = fs.existsSync(http404)
     if (exists) {
       let body = await readFile(http404, {encoding: 'utf8'})
