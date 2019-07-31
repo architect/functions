12b2f187f1d2f6197e288f3a1d4d8a1102044c14
filmaj <maj.fil@gmail.com>
2019-07-31 12:32:47 -0500

    ✅📝 api docs for queues, and added queues.publish unit tests.

---
+++ test/unit/queues/publish-test.js
@@ -0,0 +1,12 @@
+let test = require('tape')
+let publish = require('../../../src/queues/publish')
+
+test('queues.publish should throw if there is no parameter name', t => {
+  t.plan(1)
+  t.throws(() => { publish({}) }, /missing params.name/, 'throws missing name parameter exception')
+})
+
+test('queues.publish should throw if there is no parameter payload', t => {
+  t.plan(1)
+  t.throws(() => { publish({name: 'batman'})}, /missing params.payload/, 'throws missing payload parameter exception')
+})
