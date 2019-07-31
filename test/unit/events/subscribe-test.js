8bafedb9ee524bcbab1ee284fcf33bab90274037
filmaj <maj.fil@gmail.com>
2019-07-31 12:20:22 -0500

    ✅📝 api docs for events, and added events.subscribe unit tests.

---
+++ test/unit/events/subscribe-test.js
@@ -0,0 +1,31 @@
+let test = require('tape')
+let sinon = require('sinon')
+let subscribe = require('../../../src/events/subscribe')
+
+test('events.subscribe should invoke provided handler for each SNS event Record', t => {
+  t.plan(2)
+  let fake = sinon.fake.yields()
+  let handler = subscribe(fake)
+  handler({
+    Records: [{Sns:{Message:'{"hey":"there"}'}}, {Sns:{Message:'{"sup":"bud"}'}}]
+  }, {}, function(err) {
+    if (err) t.fail(err)
+    else {
+      t.ok(fake.calledWith({hey:"there"}), 'subscribe handler called with first SNS record')
+      t.ok(fake.calledWith({sup:"bud"}), 'subscribe handler called with second SNS record')
+    }
+  })
+})
+
+test('events.subscribe should invoke provided handler for each SNS event Record when handler is async', async t => {
+  t.plan(2)
+  let fake = sinon.fake()
+  let handler = subscribe(async function(json) {
+    fake(json)
+  })
+  await handler({
+    Records: [{Sns:{Message:'{"hey":"there"}'}}, {Sns:{Message:'{"sup":"bud"}'}}]
+  })
+  t.ok(fake.calledWith({hey:"there"}), 'subscribe handler called with first SNS record')
+  t.ok(fake.calledWith({sup:"bud"}), 'subscribe handler called with second SNS record')
+})
