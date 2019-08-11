70d3737998950b2991f754a8abee5606819703ab
Brian LeRoux <b@brian.io>
2019-08-10 22:07:08 -0700

    support Content-Type and content-type header for bodyParser helper

--- src/http/helpers/body-parser.js
+++ src/http/helpers/body-parser.js
@@ -5,22 +5,26 @@ let qs = require('querystring')
  * <Arc 6 bodies are always objects
  */
 module.exports = function parseBody (req) {
-  if (!req.body || !req.headers || !req.headers['Content-Type'] || !Object.getOwnPropertyNames(req.body).length) return req.body
+  let ctype = req.headers['Content-Type'] || req.headers['content-type']
+  let passthru = !req.body || !req.headers || !ctype || !Object.getOwnPropertyNames(req.body).length
+  if (passthru) {
+    return req.body
+  }
   else {
     // Paranoid deep copy
     let request = JSON.parse(JSON.stringify(req))
-    // Predicates
+    let headers = request.headers
+    let contentType = type => headers && headers['Content-Type'] && headers['Content-Type'].includes(type) || headers && headers['content-type'] && headers['content-type'].includes(type)
+
     let isString = typeof request.body === 'string'
     let isBase64 = request.isBase64Encoded
-    let shouldParse = isString && isBase64
-    // Content types
-    let contentType = type => request.headers && request.headers['Content-Type'].includes(type)
-    let isJson = contentType('application/json') && shouldParse
-    let isFormURLEncoded = contentType('application/x-www-form-urlencoded') && shouldParse
-    let isMultiPartFormData = contentType('multipart/form-data') && shouldParse
-    let isOctetStream = contentType('application/octet-stream') && shouldParse
+    let isParsing = isString && isBase64
+    let isJSON = contentType('application/json') && isParsing
+    let isFormURLEncoded = contentType('application/x-www-form-urlencoded') && isParsing
+    let isMultiPartFormData = contentType('multipart/form-data') && isParsing
+    let isOctetStream = contentType('application/octet-stream') && isParsing
 
-    if (isJson) {
+    if (isJSON) {
       try {
         // Handles base64 + JSON-encoded payloads (>Arc 6)
         let data = new Buffer.from(request.body, 'base64').toString()
@@ -31,20 +35,16 @@ module.exports = function parseBody (req) {
       }
     }
 
-    else if (isFormURLEncoded) {
+    if (isFormURLEncoded) {
       let data = new Buffer.from(request.body, 'base64').toString()
       request.body = qs.parse(data)
     }
 
-    else if (isMultiPartFormData || isOctetStream) {
+    if (isMultiPartFormData || isOctetStream) {
       request.body = request.body.base64
         ? request.body
         : {base64: request.body}
     }
-
-    // TODO maybe?
-    // - text/plain
-    // - application/xml
 
     return request.body
   }
