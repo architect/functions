b71eb38bafdd40d07c1d2341b3d90086fd9caecf
Brian LeRoux <b@brian.io>
2019-08-20 09:58:47 -0700

    fixes local arc.tables

--- src/tables/read-arc.js
+++ src/tables/read-arc.js
@@ -2,6 +2,7 @@ let fs = require('fs')
 let path = require('path')
 let parse = require('@architect/parser')
 
+// TODO easy improvement…make this readJSON and readYAML one method
 function read(filepath, callback) {
   fs.readFile(filepath, {encoding:'utf8'}, function done(err, result) {
     if (err) callback(err)
@@ -19,6 +20,40 @@ function read(filepath, callback) {
   })
 }
 
+function readJSON(filepath, callback) {
+  fs.readFile(filepath, {encoding:'utf8'}, function done(err, result) {
+    if (err) callback(err)
+    else {
+      let err
+      let arc
+      try {
+        arc = parse.json(result.toString())
+      }
+      catch(e) {
+        err = e
+      }
+      callback(err, arc)
+    }
+  })
+}
+
+function readYAML(filepath, callback) {
+  fs.readFile(filepath, {encoding:'utf8'}, function done(err, result) {
+    if (err) callback(err)
+    else {
+      let err
+      let arc
+      try {
+        arc = parse.yaml(result.toString())
+      }
+      catch(e) {
+        err = e
+      }
+      callback(err, arc)
+    }
+  })
+}
+
 /**
  * reads the .arc file
  */
@@ -27,6 +62,10 @@ module.exports = function readArc(callback) {
   let arcDefault = path.join(process.cwd(), 'node_modules', '@architect', 'shared', '.arc')
   let arcInCurrentDir = path.join(process.cwd(), '.arc')
   let arcInSharedDir = path.join(__dirname, '..', 'shared', '.arc')
+  let arcInRoot = path.join(process.cwd(), '..', '..', '..', '.arc')
+  let appArcInRoot = path.join(process.cwd(), '..', '..', '..', 'app.arc')
+  let jsonInRoot = path.join(process.cwd(), '..', '..', '..', 'arc.json')
+  let yamlInRoot = path.join(process.cwd(), '..', '..', '..', 'arc.yaml')
 
   if (fs.existsSync(arcDefault)) {
     // Arc default path (used in Arc 4 as well as ARC_LOCAL)
@@ -40,6 +79,20 @@ module.exports = function readArc(callback) {
     // Otherwise we are: testing, staging, or in production and loading from within node_modules
     // Eg, ./node_modules/@architect/shared/.arc
     read(arcInSharedDir, callback)
+  }
+  else if (fs.existsSync(arcInRoot)) {
+    // arc is in the root of the project (Arc 6.x local)
+    read(arcInRoot, callback)
+  }
+  else if (fs.existsSync(appArcInRoot)) {
+    // app.arc is in the root of the project (Arc 6.x local)
+    read(arcInRoot, callback)
+  }
+  else if (fs.existsSync(jsonInRoot)) {
+    readJSON(jsonInRoot, callback)
+  }
+  else if (fs.existsSync(yamlInRoot)) {
+    readYAML(yamlInRoot, callback)
   }
   else {
     callback(ReferenceError('.arc file not found'))
