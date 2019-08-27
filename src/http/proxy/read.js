13c594128b189bccd06a98d33c24f00326075577
Ryan Block <rblock@gmail.com>
2019-08-27 15:56:51 -0700

    Fix binary asset transfers, enable custom headers

--- src/http/proxy/read.js
+++ src/http/proxy/read.js
@@ -58,30 +58,52 @@ module.exports = async function read({Bucket, Key, IfNoneMatch, config}) {
     // No ETag found, return the blob
     if (!matchedETag) {
 
-      let type = mime.contentType(path.extname(Key))
-      let body = result.Body.toString()//('base64')
-
-      headers.ETag = result.ETag
-      headers['content-type'] = result.ContentType || type
-
-      let neverCache = noCache.some(n => type.startsWith(n))
-      if (neverCache) {
-        headers['cache-control'] = 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-      }
-      else {
-        headers['cache-control'] = 'max-age=86400'
-      }
-
+      // Transform first to allow for any proxy plugin mutations
       response = transform({
-        Key,       // TODO rename to file
-        config,    //
-        defaults: {// TODO rename to response
+        Key,         // TODO rename to file
+        config,
+        defaults: {  // TODO rename to response
           headers,
-          body,
+          body: result.Body.toString()
         },
       })
-    }
 
+      // Base64 everything on the way out to enable binary support
+      response.body = Buffer.from(response.body).toString('base64')
+      response.isBase64Encoded = true
+
+      /**
+       * Headers
+       */
+      // ETag
+      response.headers.ETag = result.ETag
+
+      // Content-Type
+      let contentType = response.headers['Content-Type'] || // Possibly get content-type passed via proxy plugins
+                        response.headers['content-type'] || // ...
+                        result.ContentType ||               // Fall back to what came down from S3's metadata
+                        mime.contentType(path.extname(Key)) // Finally, fall back to the mime type database
+      response.headers['Content-Type'] = contentType
+      let neverCache = noCache.some(n => contentType.includes(n))
+      if (neverCache)
+        response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+      else
+        response.headers['Cache-Control'] = 'max-age=86400'
+
+      // Populate optional configured headers
+      if (config.headers)
+        Object.keys(config.headers).forEach(h => headers[h] = response.headers[h])
+
+      // Normalize important common header casings to prevent dupes
+      if (response.headers['content-type']) {
+        response.headers['Content-Type'] = response.headers['content-type']
+        delete response.headers['content-type']
+      }
+      if (response.headers['cache-control']) {
+        response.headers['Cache-Control'] = response.headers['cache-control']
+        delete response.headers['cache-control']
+      }
+    }
     return response
   }
   catch(e) {
