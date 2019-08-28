8a7bee576911c31a2aa2fadd86c49b5494e1bbbe
Ryan Block <rblock@gmail.com>
2019-08-27 17:24:19 -0700

    Comments

--- src/http/proxy/read.js
+++ src/http/proxy/read.js
@@ -81,19 +81,23 @@ module.exports = async function read({Bucket, Key, IfNoneMatch, config}) {
       // ETag
       response.headers.ETag = result.ETag
 
-      // Content-Type
-      let contentType = response.headers['Content-Type'] || // Possibly get content-type passed via proxy plugins
-                        response.headers['content-type'] || // ...
-                        result.ContentType ||               // Fall back to what came down from S3's metadata
-                        mime.contentType(path.extname(Key)) // Finally, fall back to the mime type database
+      // Establish Content-Type
+      let contentType =
+        response.headers['Content-Type'] || // Possibly get content-type passed via proxy plugins
+        response.headers['content-type'] || // ...
+        result.ContentType ||               // Fall back to what came down from S3's metadata
+        mime.contentType(path.extname(Key)) // Finally, fall back to the mime type database
+      // Set Content-Type
       response.headers['Content-Type'] = contentType
+
+      // Set caching headers
       let neverCache = noCache.some(n => contentType.includes(n))
       if (neverCache)
         response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
       else
         response.headers['Cache-Control'] = 'max-age=86400'
 
-      // Populate optional configured headers
+      // Populate optional userland headers
       if (config.headers)
         Object.keys(config.headers).forEach(h => headers[h] = response.headers[h])
 
