b9906343e66b1e6038c2e36b41ffe6202c5d4b88
Ryan Block <rblock@gmail.com>
2019-10-19 16:08:54 -0700

    Throw on missing asset path

--- src/static/index.js
+++ src/static/index.js
@@ -2,6 +2,13 @@ const {readFileSync, existsSync} = require('fs')
 const {join} = require('path')
 
 /**
+ * Architect static asset helper
+ * - Returns the live asset filename
+ *
+ * In order to keep this method sync, it does not use reflection to get fingerprint status
+ * - Not checking @static fingerprint true (which we used to read from the .arc file) is possibly dangerous, so ensure asset path is valid
+ * - ? TODO: add fingerprint state to env vars in Arc 6 to restore safety in that configuration?
+ *
  * @param {string} path - the path to the asset (eg. /index.js)
  * @returns {string} path - the resolved asset path (eg. /_static/index-xxx.js)
  */
@@ -13,7 +20,10 @@ module.exports = function _static(path) {
   if (!local && exists) {
     let read = p=> readFileSync(p).toString()
     let pkg = JSON.parse(read(manifest))
-    return `/_static/${pkg[key]}`
+    let asset = pkg[key]
+    if (!asset)
+      throw ReferenceError('Could not find asset in static.json (asset fingerprint manifest)')
+    return `/_static/${asset}`
   }
   return `/_static/${key}`
 }
