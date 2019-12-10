f7a75ed79c81dc41ffa65e4c0dd6c549324d83ed
Ryan Block <rblock@gmail.com>
2019-12-10 13:44:09 -0800

    Add custom port to arc.ws

--- src/ws/send-sandbox.js
+++ src/ws/send-sandbox.js
@@ -1,10 +1,11 @@
 let http = require('http')
 
 module.exports = function send({id, payload}, callback) {
+  let port = process.env.PORT || 3333
   let body = JSON.stringify({id, payload})
   let req = http.request({
     method: 'POST',
-    port: 3333,
+    port,
     path: '/__arc',
     headers: {
       'Content-Type': 'application/json',
