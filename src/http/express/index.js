a45f6997b00144385b72a11b3409a9e864b17eb3
Brian LeRoux <b@brian.io>
2020-02-10 19:34:54 -0800

    replace ci with i

--- src/http/express/index.js
+++ src/http/express/index.js
@@ -3,7 +3,7 @@ const fmt = require('./fmt')
 
 module.export = function unexpress(app) {
   let server = aws.createServer(app)
-  return function http(event, context, callback) { 
+  return function http(event, context, callback) {
     let request = fmt(event)
     return aws.proxy(server, request, context, 'CALLBACK', callback)
   }
