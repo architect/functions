65efd214b6339dc57681d0a839ca9dbb71f7716d
Brian LeRoux <b@brian.io>
2020-02-10 19:40:29 -0800

    typo

--- src/http/express/index.js
+++ src/http/express/index.js
@@ -1,7 +1,7 @@
 const aws = require('aws-serverless-express')
 const fmt = require('./fmt')
 
-module.export = function unexpress(app) {
+module.exports = function unexpress(app) {
   let server = aws.createServer(app)
   return function http(event, context, callback) {
     let request = fmt(event)
