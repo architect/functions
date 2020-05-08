3ad9c0f698c32ceaf902391a5dd1a8bf47bcf18f
Brian LeRoux <b@brian.io>
2020-05-07 20:14:49 -0700

    fix express in sandbox

--- src/http/express/index.js
+++ src/http/express/index.js
@@ -1,12 +1,13 @@
 const aws = require('aws-serverless-express')
-const fmt = require('./fmt')
 
 module.exports = function unexpress(app) {
   let server = aws.createServer(app)
   return function http(event, context, callback) {
-    let request = fmt(event)
-    if (process.env.NODE_ENV === 'testing' || process.env.ARC_LOCAL)
+    if (process.env.NODE_ENV === 'testing' || process.env.ARC_LOCAL) {
       return aws.proxy(server, request, context, 'CALLBACK', callback)
-    return aws.proxy(server, request, context)
+    }
+    else {
+      return aws.proxy(server, request, context)
+    }
   }
 }
