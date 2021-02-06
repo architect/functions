396dcc64b3686b57b493b2aba91a07d8ccbfbb85
Joe Fleming <github@joefleming.net>
2021-02-05 17:08:19 -0700

    fix: add httpMethod to event object

--- src/http/express/index.js
+++ src/http/express/index.js
@@ -3,6 +3,8 @@ const aws = require('aws-serverless-express')
 module.exports = function unexpress (app) {
   let server = aws.createServer(app)
   return function http (event, context, callback) {
+    const [ method ] = event.routeKey.split(' ')
+    event.httpMethod = method
     if (process.env.NODE_ENV === 'testing' || process.env.ARC_LOCAL) {
       return aws.proxy(server, event, context, 'CALLBACK', callback)
     }
