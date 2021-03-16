41df5e9d4e9f4500d371a2040d5b9d36ca4dbaad
filmaj <maj.fil@gmail.com>
2021-03-16 14:03:30 -0400

    prototyping generalized service discovery

--- src/queues/publish-queue.js
+++ src/queues/publish-queue.js
@@ -1,36 +1,34 @@
 let aws = require('aws-sdk')
-let lookup = require('../discovery')
 let ledger = {}
 
-module.exports = function live ({ name, payload, delaySeconds, groupID }, callback) {
+module.exports = function liveFactory (services) {
+  return function live ({ name, payload, delaySeconds, groupID }, callback) {
 
-  function publish (QueueUrl, payload, callback) {
-    let sqs = new aws.SQS
-    let params = {
-      QueueUrl,
-      DelaySeconds: delaySeconds || 0,
-      MessageBody: JSON.stringify(payload)
+    function publish (QueueUrl, payload, callback) {
+      let sqs = new aws.SQS
+      let params = {
+        QueueUrl,
+        DelaySeconds: delaySeconds || 0,
+        MessageBody: JSON.stringify(payload)
+      }
+      if (QueueUrl.endsWith('.fifo')) {
+        params.MessageGroupId = groupID || name
+      }
+      sqs.sendMessage(params, callback)
     }
-    if (QueueUrl.endsWith('.fifo')) {
-      params.MessageGroupId = groupID || name
-    }
-    sqs.sendMessage(params, callback)
-  }
 
-  let arn = ledger[name]
-  if (arn) {
-    publish(ledger[name], payload, callback)
-  }
-  else {
-    lookup.queues(function done (err, found) {
-      if (err) callback(err)
-      else if (!found[name]) {
-        callback(ReferenceError(`${name} not found`))
-      }
-      else {
-        ledger = found
-        publish(ledger[name], payload, callback)
-      }
-    })
+    let arn = ledger[name]
+    if (arn) {
+      publish(ledger[name], payload, callback)
+    }
+    else {
+      services.then(function (serviceMap) {
+        if (!serviceMap.queues[name]) callback(ReferenceError(`${name} queue not found`))
+        else {
+          ledger = serviceMap.queues
+          publish(ledger[name], payload, callback)
+        }
+      }).catch(callback)
+    }
   }
 }
--- src/tables/index.js
+++ src/tables/index.js
@@ -18,51 +18,60 @@ let client = false
  *  return {statusCode: 200}
  * }
  */
-function tables (callback) {
-  let promise
-  if (!callback) {
-    promise = new Promise(function ugh (res, rej) {
-      callback = function errback (err, result) {
-        if (err) rej(err)
-        else res(result)
-      }
-    })
+function tables (services) {
+  let api = function (callback) {
+    let promise
+    if (!callback) {
+      promise = new Promise(function ugh (res, rej) {
+        callback = function errback (err, result) {
+          if (err) rej(err)
+          else res(result)
+        }
+      })
+    }
+    /**
+     * Read Architect manifest if local / sandbox, otherwise use service reflection
+     */
+    let runningLocally = process.env.NODE_ENV === 'testing'
+    if (runningLocally) {
+      sandbox(callback)
+    }
+    else if (client) {
+      callback(null, client)
+    }
+    else {
+      waterfall([
+        function (callback) {
+          services.then(function (serviceMap) {
+            console.log('tables instantiation, service map returned', serviceMap)
+            callback(null, serviceMap.tables)
+          }).catch(callback)
+        },
+        factory,
+        function (created, callback) {
+          client = created
+          callback(null, client)
+        }
+      ], callback)
+    }
+    return promise
   }
-  /**
-   * Read Architect manifest if local / sandbox, otherwise use service reflection
-   */
-  let runningLocally = process.env.NODE_ENV === 'testing'
-  if (runningLocally) {
-    sandbox(callback)
-  }
-  else if (client) {
-    callback(null, client)
-  }
-  else {
-    waterfall([
-      lookup.tables,
-      factory,
-      function (created, callback) {
-        client = created
-        callback(null, client)
-      }
-    ], callback)
-  }
-  return promise
+  // Export directly for fast use
+  api.doc = dynamo.direct.doc
+  api.db = dynamo.direct.db
+
+  // Legacy compat methods
+  api.insert = old.insert
+  api.modify = old.modify
+  api.update = old.update
+  api.remove = old.remove
+  api.destroy = old.destroy
+  api.all = old.all
+  api.save = old.save
+  api.change = old.change
+  api() // kick off fetching and caching the table client pre-emptively
+  return api
 }
 
-// Export directly for fast use
-tables.doc = dynamo.direct.doc
-tables.db = dynamo.direct.db
-
-// Legacy compat methods
-tables.insert = old.insert
-tables.modify = old.modify
-tables.update = old.update
-tables.remove = old.remove
-tables.destroy = old.destroy
-tables.all = old.all
-tables.save = old.save
-tables.change = old.change
 
 module.exports = tables
