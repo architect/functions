5ee9dcc940696967e4e13d50883c34f7351771d8
filmaj <maj.fil@gmail.com>
2021-03-16 14:03:30 -0400

    prototyping generalized service discovery

--- src/events/index.js
+++ src/events/index.js
@@ -1,36 +1,37 @@
 let oldPublish = require('./publish-old')
-let publish = require('./publish')
 let subscribe = require('./subscribe')
 
-module.exports = {
+module.exports = function eventFactory (services) {
+  let publish = require('./publish')(services)
+  return {
+    /**
+     * arc.events.publish
+     *
+     * publish events (sns topics)
+     *
+     * @param {Object} params
+     * @param {String} params.name - the event name (required)
+     * @param {String} params.payload - a json event payload (required)
+     * @param {Function} callback - a node style errback (optional)
+     * @returns {Promise} - returned if no callback is supplied
+     */
+    publish (params, callback) {
+      if (process.env.ARC_CLOUDFORMATION) {
+        return publish(params, callback)
+      }
+      else {
+        return oldPublish(params, callback)
+      }
+    },
 
-  /**
-   * arc.events.publish
-   *
-   * publish events (sns topics)
-   *
-   * @param {Object} params
-   * @param {String} params.name - the event name (required)
-   * @param {String} params.payload - a json event payload (required)
-   * @param {Function} callback - a node style errback (optional)
-   * @returns {Promise} - returned if no callback is supplied
-   */
-  publish (params, callback) {
-    if (process.env.ARC_CLOUDFORMATION) {
-      return publish(params, callback)
-    }
-    else {
-      return oldPublish(params, callback)
-    }
-  },
-
-  /**
-   * arc.events.subscribe
-   *
-   * listen for events (sns topics)
-   *
-   * @param {Function} handler - a single event handler function
-   * @returns {Lambda} - a Lambda function sig: (event, context, callback)=>
-   */
-  subscribe,
+    /**
+     * arc.events.subscribe
+     *
+     * listen for events (sns topics)
+     *
+     * @param {Function} handler - a single event handler function
+     * @returns {Lambda} - a Lambda function sig: (event, context, callback)=>
+     */
+    subscribe
+  }
 }
--- src/events/publish.js
+++ src/events/publish.js
@@ -1,28 +1,29 @@
 let sandbox = require('./publish-sandbox')
-let topic = require('./publish-topic')
 
 /**
  * invoke an event lambda by sns topic name
  */
-module.exports = function publish (params, callback) {
+module.exports = function publishFactory (services) {
+  let topic = require('./publish-topic')(services)
+  return function publish (params, callback) {
+    if (!params.name)
+      throw ReferenceError('missing params.name')
 
-  if (!params.name)
-    throw ReferenceError('missing params.name')
+    if (!params.payload)
+      throw ReferenceError('missing params.payload')
 
-  if (!params.payload)
-    throw ReferenceError('missing params.payload')
+    let promise
+    if (!callback) {
+      promise = new Promise((resolve, reject) => {
+        callback = function errback (err, result) {
+          err ? reject(err) : resolve(result)
+        }
+      })
+    }
 
-  let promise
-  if (!callback) {
-    promise = new Promise((resolve, reject) => {
-      callback = function errback (err, result) {
-        err ? reject(err) : resolve(result)
-      }
-    })
+    let isLocal = process.env.NODE_ENV === 'testing' || process.env.ARC_LOCAL
+    let exec = isLocal ? sandbox : topic
+    exec(params, callback)
+    return promise
   }
-
-  let isLocal = process.env.NODE_ENV === 'testing' || process.env.ARC_LOCAL
-  let exec = isLocal ? sandbox : topic
-  exec(params, callback)
-  return promise
 }
