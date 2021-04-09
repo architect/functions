e5689ff3f9ffd0201d1ab58fcddd3ca1afb689e3
filmaj <maj.fil@gmail.com>
2021-04-09 12:57:47 -0400

    use sandbox mock SSM for service discovery when running locally

--- src/discovery/index.js
+++ src/discovery/index.js
@@ -5,38 +5,24 @@ let http = require('http')
  * @returns {object} {name: value}
  */
 module.exports = function lookup (callback) {
-  if (process.env.NODE_ENV === 'testing') lookupSandbox(callback)
-  else lookupSSM(callback)
-}
-
-function lookupSandbox (callback) {
-  let port = process.env.PORT || 3333
-  let req = http.request({
-    method: 'GET',
-    port,
-    path: '/_asd',
-  },
-  function done (res) {
-    let data = []
-    res.resume()
-    res.on('data', chunk => data.push(chunk))
-    res.on('end', () => {
-      let body = Buffer.concat(data).toString()
-      let code = `${res.statusCode}`
-      if (!code.startsWith(2)) callback(Error(`_asd error; (${code}) ${body}`))
-      else callback(null, JSON.parse(body))
-    })
-  })
-  req.end('\n')
-}
-
-function lookupSSM (callback) {
   let Path = `/${process.env.ARC_CLOUDFORMATION}`
   let Recursive = true
   let values = []
+  let isLocal = process.env.NODE_ENV === 'testing'
+  let config
+  if (isLocal) {
+    // if running in sandbox, sandbox has an SSM mock, use that
+    let port = process.env.ARC_INTERNAL || 3332
+    let region = process.env.AWS_REGION || 'us-west-2'
+    config = {
+      endpoint: new aws.Endpoint(`http://localhost:${port}/_arc/ssm`),
+      region,
+      httpOptions: { agent: new http.Agent() }
+    }
+  }
+  let ssm = new aws.SSM(config)
 
   function getParams (params) {
-    let ssm = new aws.SSM
     ssm.getParametersByPath(params, function done (err, result) {
       if (err) {
         callback(err)
