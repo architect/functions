6baee67b4c3ceef7051344594cf1149f5c36f043
filmaj <maj.fil@gmail.com>
2021-04-13 12:17:13 -0400

    changing up the services api

--- src/queues/publish-queue.js
+++ src/queues/publish-queue.js
@@ -27,14 +27,8 @@ module.exports = function liveFactory (arc) {
     if (arn) {
       publish(ledger[name], payload, callback)
     }
-    else if (!arc.services) {
-      // lazy load service map if not fetched yet
-      console.log('lazy loading queues')
-      arc._loadServices().then(cacheLedgerAndPublish).catch(callback)
-    }
     else {
-      // services were loaded before, set up queue ledger / cache
-      cacheLedgerAndPublish(arc.services)
+      arc.services().then(cacheLedgerAndPublish).catch(callback)
     }
   }
 }
