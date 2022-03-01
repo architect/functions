a709beb41af3150fcbb374af0dfa58497e1224a3
Ryan Block <rblock@gmail.com>
2022-03-01 14:38:04 -0800

    Fix usage with custom stack names; fixes #1322 Remove unnecessary http agent instantiation

--- src/tables/dynamo.js
+++ src/tables/dynamo.js
@@ -4,8 +4,6 @@ let db, doc
 
 /**
  * Instantiates Dynamo service interfaces
- * - Internal APIs should use `db` + `doc` to instantiate DynamoDB interfaces
- * - Avoid using `direct.db` + `direct.doc`: as it's an issue vector for using Functions in certain test harnesses!
  */
 function getDynamo (type, callback) {
   if (!type) throw ReferenceError('Must supply Dynamo service interface type')
@@ -14,13 +12,7 @@ function getDynamo (type, callback) {
   // eslint-disable-next-line
   let dynamo = require('aws-sdk/clients/dynamodb')
 
-  // We might normally like to throw if `local && !port`, but this is also a direct DynamoDB interface in global scope
-  // Thus, this path instantiates even if the project doesn't have tables
-  let {
-    ARC_ENV,
-    ARC_LOCAL,
-    AWS_REGION,
-  } = process.env
+  let { ARC_ENV, ARC_LOCAL, AWS_REGION } = process.env
   let local = ARC_ENV === 'testing' || ARC_LOCAL
   let DB = dynamo
   let Doc = dynamo.DocumentClient
@@ -32,23 +24,16 @@ function getDynamo (type, callback) {
     return callback(null, doc)
   }
 
-  /**
-   * This module may be loaded by @arc/arc via repl
-   * - The `direct` interfaces will instantiate before ARC_ENV is set
-   * - Thus, unlike most other scenarios, don't assume the presence of ARC_ENV
-   * - Also: some test harnesses (ahem) will automatically populate ARC_ENV with their own values, unbidden
-   * - *Why this matters*: using https.Agent (and not http.Agent) will stall the Sandbox
-   */
   if (!local) {
     let agent = new https.Agent({
       keepAlive: true,
       maxSockets: 50, // Node can set to Infinity; AWS maxes at 50; check back on this every once in a while
       rejectUnauthorized: true,
     })
+    // TODO? migrate to using `AWS_NODEJS_CONNECTION_REUSE_ENABLED`?
     let config = {
       httpOptions: { agent }
     }
-    // TODO? migrate to using `AWS_NODEJS_CONNECTION_REUSE_ENABLED`?
 
     if (type === 'db') {
       db = new DB(config)
