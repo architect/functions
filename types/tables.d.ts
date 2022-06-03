20aeb6722f708c21243351d591a6f1bdfd87259a
Cam Jackson <1930451+camjackson@users.noreply.github.com>
2022-06-03 20:51:08 +1000

    Minor tweaks / corrections to tables typings

--- types/tables.d.ts
+++ types/tables.d.ts
@@ -3,21 +3,19 @@ import type { DynamoDB } from "aws-sdk";
 // TableName not needed as the library sets it
 type Params<InputType> = Omit<InputType, "TableName">;
 
-// TableName not needed as the library sets it
-// Plus, we know the type of the returned items
-type Output<OutputType, Item> = Omit<OutputType, "TableName" | "Items"> & {
-  Items: Item;
+// Strip and then override the Items field
+type ItemsOutput<OutputType, Item> = Omit<OutputType, "Items"> & {
+  Items: Item[];
 };
 
 type QueryParams = Params<DynamoDB.DocumentClient.QueryInput>;
-type QueryOutput<Item> = Output<DynamoDB.DocumentClient.QueryOutput, Item>;
+type QueryOutput<Item> = ItemsOutput<DynamoDB.DocumentClient.QueryOutput, Item>;
+
 type ScanParams = Params<DynamoDB.DocumentClient.ScanInput>;
-type ScanOutput<Item> = Output<DynamoDB.DocumentClient.ScanOutput, Item>;
+type ScanOutput<Item> = ItemsOutput<DynamoDB.DocumentClient.ScanOutput, Item>;
+
 type UpdateParams = Params<DynamoDB.DocumentClient.UpdateItemInput>;
-type UpdateOutput<Item> = Output<
-  DynamoDB.DocumentClient.UpdateItemOutput,
-  Item
->;
+type UpdateOutput = DynamoDB.DocumentClient.UpdateItemOutput;
 
 // Depending on the operation, the key attributes may be mandatory, but we don't
 // know what the key attributes are, so Partial is the best we can do.
@@ -41,8 +39,8 @@ export interface ArcTable<Item = unknown> {
   scan(params: ScanParams): Promise<ScanOutput<Item>>;
   scan(params: ScanParams, callback: Callback<ScanOutput<Item>>): void;
 
-  update(params: UpdateParams): Promise<UpdateOutput<Item>>;
-  update(params: UpdateParams, callback: Callback<UpdateOutput<Item>>): void;
+  update(params: UpdateParams): Promise<UpdateOutput>;
+  update(params: UpdateParams, callback: Callback<UpdateOutput>): void;
 }
 
 type DB<Tables> = {
@@ -52,8 +50,8 @@ type DB<Tables> = {
 // Permissive by default: allows any table, any inputs, any outputs.
 type AnyTables = Record<string, any>;
 
-export interface ArcTables<Tables = AnyTables> {
-  (): Promise<DB<Tables>>;
+export interface ArcTables {
+  <Tables = AnyTables>(): Promise<DB<Tables>>;
 
   // legacy methods
   insert: any;
