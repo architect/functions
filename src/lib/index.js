1f6d98133c2b6e23846105b9df3a38dfb221b574
Ryan Block <rblock@gmail.com>
2023-02-13 12:42:17 -0800

    Ensure tests don't get a `useAWS` false-negative

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,10 +5,13 @@ let isNode18 = Number(process.version.replace('v', '').split('.')[0]) >= 18
 
 let nonLocalEnvs = [ 'staging', 'production' ]
 function useAWS () {
-  let { ARC_ENV, ARC_LOCAL } = process.env
+  let { ARC_ENV, ARC_LOCAL, ARC_SANDBOX } = process.env
+  // Testing is always local
   if (ARC_ENV === 'testing') return false
-  if (nonLocalEnvs.includes(ARC_ENV) && ARC_LOCAL) return true
-  return false
+  // Local, but using AWS resources
+  if (nonLocalEnvs.includes(ARC_ENV) && ARC_SANDBOX && !ARC_LOCAL) return false
+  // Assumed to be AWS
+  return true
 }
 
 module.exports = {
