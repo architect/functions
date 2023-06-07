f80010f2d7619f1ce34d9d6770e64e6f1122ebdc
Ryan Block <rblock@gmail.com>
2023-06-06 20:01:27 -0700

    Fixed issue where `arc.tables` could keep Sandbox Dynamo connections open for 5s in Node 18+ Update deps and changelog

--- src/tables/dynamo.js
+++ src/tables/dynamo.js
@@ -1,4 +1,3 @@
-let https = require('https')
 let { getPorts, isNode18, useAWS } = require('../lib')
 
 /**
@@ -35,16 +34,21 @@ function getDynamo (type, callback) {
   }
 
   if (useAWS()) {
-    let config = {
-      httpOptions: {
-        agent: new https.Agent({
-          keepAlive: true,
-          maxSockets: 50, // Node can set to Infinity; AWS maxes at 50
-          rejectUnauthorized: true,
-        })
+    let config
+    // SDK v2 (Node <=16) does not have keep-alive enabled by default, whereas v3 (>=18) does
+    if (!isNode18) {
+      let https = require('https')
+      config = {
+        httpOptions: {
+          agent: new https.Agent({
+            keepAlive: true,
+            maxSockets: 50, // Node can set to Infinity; AWS maxes at 50
+            rejectUnauthorized: true,
+          })
+        }
       }
     }
-    db = isNode18 ? new DB : new DB(config)
+    db = new DB(config)
     doc = isNode18 ? Doc.from(db) : new Doc(config)
     return callback(null, type === 'db' ? db : doc)
   }
@@ -59,6 +63,14 @@ function getDynamo (type, callback) {
         let config = {
           endpoint: `http://localhost:${port}`,
           region: AWS_REGION || 'us-west-2' // Do not assume region is set!
+        }
+        if (isNode18) {
+          // Disable keep-alive locally (or wait Node's default 5s for sockets to time out)
+          let http = require('http')
+          let { NodeHttpHandler } = require('@aws-sdk/node-http-handler')
+          config.requestHandler = new NodeHttpHandler({
+            httpAgent: new http.Agent({ keepAlive: false })
+          })
         }
         db = new DB(config)
         doc = isNode18 ? Doc.from(db) : new Doc(config)
