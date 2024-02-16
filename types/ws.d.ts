ce954f6a9525497cb3656976899b340dcc768d99
Taylor Beseda <tbeseda@gmail.com>
2024-02-16 14:33:24 -0700

    tables and ws types are inherited from aws-lite _plugins_
    relying on the client's ambient types did not work in some TS configurations

--- types/ws.d.ts
+++ types/ws.d.ts
@@ -1,5 +1,7 @@
-import type { AwsLiteClient } from "@aws-lite/client"
-import type { GetConnectionResponse } from "@aws-lite/apigatewaymanagementapi-types";
+import type {
+  AwsLiteApiGatewayManagementApi,
+  GetConnectionResponse,
+} from "@aws-lite/apigatewaymanagementapi-types";
 import { Callback } from "./util";
 
 // Turn off automatic exporting
@@ -10,8 +12,8 @@ type CloseParams = { id: string };
 type InfoParams = { id: string };
 
 export interface ArcWebSocket {
-  _api(): Promise<AwsLiteClient["ApiGatewayManagementApi"]>;
-  _api(callback: Callback<AwsLiteClient["ApiGatewayManagementApi"]>): void;
+  _api(): Promise<AwsLiteApiGatewayManagementApi>;
+  _api(callback: Callback<AwsLiteApiGatewayManagementApi>): void;
 
   send(params: SendParams): Promise<void>;
   send(params: SendParams, callback: Callback<void>): void;
